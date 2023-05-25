f4e7d14962aa251b7e24af1ebef9eab12da5f1bd
kbeni <benedekkonya005@gmail.com>
2023-05-25 11:48:58 +0200

    4.ora vege

---
+++ Balkezesek/Balkezes.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Balkezesek
+{
+    public class Balkezes
+    {
+        string nev;
+        DateTime elsoLepes;
+        DateTime utolsoLepes;
+        int sulyFontban;
+        int magassagInchben;
+
+        public string Nev { get => nev; set => nev = value; }
+        public DateTime ElsoLepes { get => elsoLepes; set => elsoLepes = value; }
+        public DateTime UtolsoLepes { get => utolsoLepes; set => utolsoLepes = value; }
+        public int SulyFontban { get => sulyFontban; set => sulyFontban = value; }
+        public int MagassagInchben { get => magassagInchben; set => magassagInchben = value; }
+
+        public Balkezes(string sor) 
+        {
+            string[] adat = sor.Split(';');
+            Nev = adat[0];
+            ElsoLepes = DateTime.Parse(adat[1]);
+            UtolsoLepes = DateTime.Parse(adat[2]);
+            SulyFontban = int.Parse(adat[3]);
+            MagassagInchben = int.Parse(adat[4]);
+        }
+    }
+}
--- Balkezesek/Program.cs
+++ Balkezesek/Program.cs
@@ -2,7 +2,9 @@
 using System.Text;
 using System.Linq;
 using System.Collections.Generic;
-
+using Balkezesek;
+using System.IO;
+ 
 
 namespace Balkezesek
 {
@@ -10,7 +12,37 @@ namespace Balkezesek
     {
         static void Main(string[] args)
         {
+            List<Balkezes> ballista = new List<Balkezes>();
 
+            StreamReader sr = new StreamReader("balkezesek.csv", Encoding.UTF8);
+            sr.ReadLine();
+            while (!sr.EndOfStream) 
+            {
+                ballista.Add(new Balkezes(sr.ReadLine()));
+            }
+            sr.Close();
+
+            Console.WriteLine($"3. feladat: {ballista.Count}");
+
+            Console.WriteLine(new string('-', 40));
+            const double inch = 2.54;
+
+            Console.WriteLine($"4. feladat: ");
+            ballista.Where(x => x.UtolsoLepes.Year == 1999 && x.UtolsoLepes.Month == 10).ToList().ForEach(y => Console.WriteLine($"{y.Nev}, {Math.Round(y.MagassagInchben * inch, 1)} cm"));
+
+            Console.WriteLine(new string('-', 40));
+
+            Console.WriteLine("5. feladat");
+            int input = 0;
+
+            do
+            {
+                Console.WriteLine("Kérem adjon meg egy évszámot 1990 és 1999 közzött!: ");
+                if (!(int.TryParse(Console.ReadLine(), out input) && input >= 1990 && input <= 1999))
+                {
+                    Console.WriteLine("Hibás adat!");
+                }
+            } while (!(input >= 1990 && input <= 1999));
         }
     }
 }
